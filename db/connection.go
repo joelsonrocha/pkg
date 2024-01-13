acf33cb0055fc0d976ae7e9b7a20b777486b56fb
Joelson <joelsondelphi@gmail.com>
2024-01-13 10:45:54 -0300

    tratamento para a conexão

--- db/connection.go
+++ db/connection.go
@@ -10,6 +10,17 @@ import (
 )
 
 func MigrateDatabase() error {
+	postgresUrl := os.Getenv("POSTGRES_URL")
+	migrationPath := os.Getenv("MIGRATION_PATH")
+
+	if postgresUrl == "" {
+		return fmt.Errorf("POSTGRES_URL variable is not available")
+	}
+
+	if migrationPath == "" {
+		return fmt.Errorf("MIGRATION_PATH variable is not available")
+	}
+
 	conn, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
 	if err != nil {
 		return fmt.Errorf("failed to open database connection: %w", err)
@@ -22,7 +33,7 @@ func MigrateDatabase() error {
 	}
 
 	m, err := migrate.NewWithDatabaseInstance(
-		"file://./migrations",
+		"file://"+os.Getenv("MIGRATION_PATH"),
 		"postgres", driver)
 	if err != nil {
 		return fmt.Errorf("failed to create migration instance: %w", err)
@@ -36,10 +47,16 @@ func MigrateDatabase() error {
 }
 
 func OpenConnection() (*sql.DB, error) {
+	postgresUrl := os.Getenv("POSTGRES_URL")
+
+	if postgresUrl == "" {
+		return nil, fmt.Errorf("POSTGRES_URL variable is not available")
+	}
 	fmt.Println("Abrindo conexão com banco:")
 	conn, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
 	if err != nil {
 		fmt.Println("failed to open database connection:", err)
+		return nil, fmt.Errorf("failed to open database connection")
 		//panic(err)
 	}
 
