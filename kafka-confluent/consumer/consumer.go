bd9a0da25a82a722166c72fea07af3ef00a8a337
Joelson <joelsondelphi@gmail.com>
2024-01-14 19:24:05 -0300

    encapsulamento das funções coisas do kafka

--- kafka-confluent/consumer/consumer.go
+++ kafka-confluent/consumer/consumer.go
@@ -8,7 +8,52 @@ import (
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
-func NewConsumer(topics []string, groupID string, handleFuntion func(*kafka.Message)) {
+type MessageHandler interface {
+	HandleMessage(*Message)
+}
+
+type Message struct {
+	TopicPartition kafka.TopicPartition
+	Value          []byte
+	Key            []byte
+	Timestamp      time.Time
+	TimestampType  int
+	Opaque         interface{}
+	Headers        []kafka.Header
+	LeaderEpoch    *int32
+}
+
+func AdaptKafkaMessage(original *kafka.Message) *Message {
+	return &Message{
+		TopicPartition: original.TopicPartition,
+		Value:          original.Value,
+		Key:            original.Key,
+		Timestamp:      original.Timestamp,
+		TimestampType:  int(original.TimestampType),
+		Opaque:         original.Opaque,
+		Headers:        original.Headers,
+		LeaderEpoch:    original.LeaderEpoch,
+	}
+}
+
+func (m *Message) AdaptToKafkaMessage() *kafka.Message {
+	return &kafka.Message{
+		TopicPartition: m.TopicPartition,
+		Value:          m.Value,
+		Key:            m.Key,
+		Timestamp:      m.Timestamp,
+		TimestampType:  kafka.TimestampType(m.TimestampType),
+		Opaque:         m.Opaque,
+		Headers:        m.Headers,
+		LeaderEpoch:    m.LeaderEpoch,
+	}
+}
+
+type Consumer struct {
+	handler MessageHandler
+}
+
+func NewConsumer(topics []string, groupID string, handler MessageHandler) {
 	c, err := kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers": os.Getenv("KAFKA_URL"),
 		"group.id":          groupID,
@@ -21,6 +66,8 @@ func NewConsumer(topics []string, groupID string, handleFuntion func(*kafka.Mess
 
 	c.SubscribeTopics(topics, nil)
 
+	consumer := &Consumer{handler: handler}
+
 	// A signal handler or similar could be used to set this to false to break the loop.
 	run := true
 
@@ -28,7 +75,7 @@ func NewConsumer(topics []string, groupID string, handleFuntion func(*kafka.Mess
 		msg, err := c.ReadMessage(time.Second)
 		if err == nil {
 			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
-			go handleFuntion(msg)
+			go consumer.handleFuntion(msg)
 		} else if !err.(kafka.Error).IsTimeout() {
 			// The client will automatically try to recover from all errors.
 			// Timeout is not considered an error because it is raised by
@@ -40,4 +87,8 @@ func NewConsumer(topics []string, groupID string, handleFuntion func(*kafka.Mess
 	}
 
 	c.Close()
+}
+
+func (c *Consumer) handleFuntion(msg *kafka.Message) {
+	c.handler.HandleMessage(AdaptKafkaMessage(msg))
 }
