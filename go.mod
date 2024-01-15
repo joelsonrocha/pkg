b4f8c5b6d6b1dcda3283a7685bc8ce2dda6d82c5
Joelson <joelsondelphi@gmail.com>
2024-01-15 09:58:44 -0300

    trocando o kafka-confluent pelo kafka-go

--- go.mod
+++ go.mod
@@ -3,18 +3,17 @@ module github.com/joelsonrocha/pkg
 go 1.20
 
 require (
-	github.com/confluentinc/confluent-kafka-go/v2 v2.3.0
 	github.com/golang-migrate/migrate/v4 v4.17.0
+	github.com/segmentio/kafka-go v0.4.47
 )
 
 require (
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/klauspost/compress v1.15.11 // indirect
 	github.com/lib/pq v1.10.9 // indirect
-	github.com/moby/patternmatcher v0.6.0 // indirect
-	github.com/moby/sys/sequential v0.5.0 // indirect
+	github.com/pierrec/lz4/v4 v4.1.16 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	golang.org/x/net v0.20.0 // indirect
-	golang.org/x/sys v0.16.0 // indirect
 )
---
+++ kafka-go/consumer.go
@@ -0,0 +1,56 @@
+package consumer
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type Consumer struct {
+	handler func(kafka.Message)
+}
+
+func NewConsumer(ctx context.Context, topics []string, groupID string, handler func(kafka.Message)) {
+	brokers := []string{os.Getenv("KAFKA_URL")}
+
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:  brokers,
+		GroupID:  groupID,
+		Topic:    topics[0], // Assuming a single topic for simplicity
+		MinBytes: 10e3,
+		MaxBytes: 10e6,
+	})
+
+	consumer := &Consumer{handler: handler}
+
+	var wg sync.WaitGroup
+
+	for {
+		select {
+		case <-ctx.Done():
+			// Context done, exit the loop
+			wg.Wait()
+			return
+		default:
+			// Continue processing messages
+			msg, err := r.ReadMessage(ctx)
+			if err != nil {
+				fmt.Printf("Consumer error: %v\n", err)
+				break
+			}
+
+			wg.Add(1)
+			go func(msg kafka.Message) {
+				defer wg.Done()
+				consumer.handleFunction(msg)
+			}(msg)
+		}
+	}
+}
+
+func (c *Consumer) handleFunction(msg kafka.Message) {
+	c.handler(msg)
+}
